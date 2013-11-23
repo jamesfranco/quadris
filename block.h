38c0cf09b44158bd46ac35ee25b73c11939155b2
lonebutterfish <tylerbrenneman34@gmail.com>
2013-11-23 14:09:00 -0500

    up1

--- block.h
+++ block.h
@@ -1,4 +1,44 @@
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
 
+class Block {
+
+ public:
+  Block();
+  ~Block();
+  void drop();
+  void left();
+  void right();
+  void clockwise();
+  void counterclockwise();
+};
+
+class IBlock : public Block {
+
+};
+
+class JBlock : public Block {
+
+};
+
+class LBlock : public Block {
+
+};
+
+class OBlock : public Block {
+
+};
+
+class Sblock : public Block {
+
+};
+
+class ZBlock : public Block {
+
+};
+
+class Tblock : public Block {
+
+};
+
 #endif
--- main.cc
+++ main.cc
@@ -4,7 +4,15 @@
 using namespace std;
 
 
+int * board [10][15];
+for (int i = 0; i < 10; i++){
+  for (int j = 0; j < 15; j++){
+  	board[i][j] = -1;
+  }
+}
+
+
 int main (){
 
-
+  return 0;
 }
