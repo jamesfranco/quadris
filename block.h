ae907d0988380a21d3ea68c9099ee170ab2051ab
lonebutterfish <tylerbrenneman34@gmail.com>
2013-11-23 17:48:41 -0500

    collision

--- block.h
+++ block.h
@@ -1,12 +1,38 @@
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
 
+struct Posn{
+	int x;
+	int y;
+}
+
+
+//corners[0] top left
+//corners[1] bottom left
+//corners[2] bottom right
 class Block {
+  Posn bottomright;
+  Posn bottomleft;
+  Posn * position[4];
+  
+  void clockwise(){
+  	int temp = 0;
+  	for (int i=0; i++; i < 4){
+  	  position[i].x-=bottomright.x;
+  	  position[i].y-=bottomright.y;
+  	  temp=position[i].x;
+  	  position[i].x = position[i].y;
+  	  position[i].y = -temp;
+    }
+  }
 
  public:
   Block();
   ~Block();
+  boolean collision();
+  void 
   void drop();
+  void down();
   void left();
   void right();
   void clockwise();
@@ -14,7 +40,14 @@ class Block {
 };
 
 class IBlock : public Block {
-
+  IBlock(){
+  	bottomleft={0,11};
+  	bottomright={3,11};
+    this->position[0]={0,11;
+    this->position[1]={1,11};
+    this->position[2]={2,11};
+    this->position[3]=[3,11];
+  }
 };
 
 class JBlock : public Block {
--- main.cc
+++ main.cc
@@ -4,15 +4,37 @@
 using namespace std;
 
 
-int * board [10][15];
-for (int i = 0; i < 10; i++){
-  for (int j = 0; j < 15; j++){
-  	board[i][j] = -1;
+string board [10][15];
+void set(){
+  for (int i = 0; i < 10; i++){
+    for (int j = 0; j < 15; j++){
+      board[i][j] = " ";
+    }
   }
+  board[5][0] = "L";
 }
 
+int score = 0;
+int hiScore = score;
+int level = 0;
+
+void textdraw (){
+  cout << "Level: " << level << endl;
+  cout << "Score: " << score << endl;
+  cout << "HiScore: " << hiScore << endl;
+  cout << "----------" << endl;
+    for (int i = 14; i >= 0; i--){
+      for (int j = 9; j >= 0; j--){
+        cout << board[j][i];
+      }
+      cout << endl;
+    }
+  cout << "----------" << endl;
+  cout << "Next: " << endl;
+}
 
 int main (){
-
+  set();
+  textdraw();
   return 0;
 }
